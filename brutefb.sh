9cf3d12e007ddebe899e73678fb65718edc0bd62
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-27 20:21:43 +0000

    Update brutefb.sh

--- brutefb.sh
+++ brutefb.sh
@@ -4,9 +4,9 @@ clear
 export PS1="\e[0;32m[github.com/nerdhacker000]\$ \e[0m"
 echo "		FOR EDUCATIONAL PURPOSES "
 echo
-echo "		 __  __   __   __  _   _ _____ ___" 
-echo "		|__ |__) |__) |__) |   |   |  |___" 
-echo "		|   |___)|___)|   \|___|   |  |___" 
+echo "		 __  __    __    __   _   _ _____  ___" 
+echo "		|__ |__)  |__)  |__)  |   |   |   |___" 
+echo "		|   |___) |___) |   \ \___/   |   |___" 
 echo
 echo
 echo "Github:https:github.com/nerdhacker000/"
