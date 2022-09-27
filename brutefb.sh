348ae9c333939489214f19ad3ef5385a85b45d71
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-27 19:36:40 +0000

    Create brutefb.sh

--- brutefb.sh
+++ brutefb.sh
@@ -1,9 +1,11 @@
 clear
 export PS1="\e[0;32m[github.com/nerdhacker000]\$ \e[0m"
-echo " FOR EDUCATIONAL PURPOSES "
-echo "  __..__...__...__.._   _._____.___" 
-echo " |__ |__) |__) |__) |   |	  |  |___" 
-echo " |   |___)|___)|   \|___|   |  |___" 
+echo "		FOR EDUCATIONAL PURPOSES "
+echo
+echo
+echo "		 __..__...__...__.._   _._____.___" 
+echo "		|__ |__) |__) |__) |   |   |  |___" 
+echo "		|   |___)|___)|   \|___|   |  |___" 
 echo " github:https:github.com/nerdhacker000/"
 echo "More tutorials on my website: https://nerdhaka.blogspot.com"
 echo "developer: Nerd Haka
@@ -16,4 +18,4 @@ if [[ $length -eq 1 ]]
 then
   echo "Starting 6-character password attack..."
 fi
-python3 brutefb6.py
+python brutefb6.py
