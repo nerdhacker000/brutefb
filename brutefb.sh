558da5ca4f86b99b8ee27d347353435edd7807ad
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-28 07:19:22 +0000

    Update brutefb.sh

--- brutefb.sh
+++ brutefb.sh
@@ -2,20 +2,22 @@
 
 clear
 export PS1="\e[0;32m[nerdhacker000]\? \e[0m"
-echo "		FOR EDUCATIONAL PURPOSES "
+echo "	   FOR EDUCATIONAL PURPOSES "
 echo
-echo "		 __  __    __    __   _   _ _____  ___" 
-echo "		|__ |__)  |__)  |__)  |   |   |   |___" 
-echo "		|   |___) |___) |   \ \___/   |   |___" 
+echo "	    __  __    __    __   _   _ _____  ___" 
+echo "     |__ |__)  |__)  |__)  |   |   |   |___" 
+echo "     |   |___) |___) |   \ \___/   |   |___" 
 echo
 echo
 echo "	Github:https:github.com/nerdhacker000/"
 echo "	My website: https://nerdhaka.blogspot.com"
 echo "	Tool developer: Nerd Haka"
 echo
-echo "	[1] 6-character password"
-echo "	[2] 7-character password"
+echo "	[1] 6 to 8 character password"
+echo "	[2] 8 to 10 character password"
+echo " - - - - - - - - - - - - - - - - - - 
 echo "	Choose password character length to attack:"
+echo
 read length
 echo "	Initializing..."
 python $length.py
