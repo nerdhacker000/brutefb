b628ecc77fc4e725335b3568a31a1955e57c20d1
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-28 07:14:52 +0000

    Update brutefb.sh

--- brutefb.sh
+++ brutefb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clear
-export PS1="\e[0;32m[github.com/nerdhacker000]\$ \e[0m"
+export PS1="\e[0;32m[nerdhacker000]\? \e[0m"
 echo "		FOR EDUCATIONAL PURPOSES "
 echo
 echo "		 __  __    __    __   _   _ _____  ___" 
