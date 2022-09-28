6a9eb55b8804577fb129ed2bef8c388b97d713dc
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-28 11:39:13 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,16 @@
-import mechanize
-br = mechanize.Browser()
-br.open("http://mywebsite.com/messages.php?action=send")
-br.select_form(nr = 0)
-br.form['email'] = 'marccarthy3@gmail.com'
-br.form['pass'] = '. 1234@ duah. com'
-req = br.submit()
-req.read()
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+browser.get("https://facebook.com/login")
+email = browser.find_element(By.NAME, "email")
+password = browser.find_element(By.NAME, "pass")
+button = browser.find_element(By.NAME, "submit")
+email.send_keys("marccarthy3@gmail.com")
+password.send_keys(". 1234@ duah.com")
+button.click()
+if assert browser.title == "Facebook":
+	print("Yes")
